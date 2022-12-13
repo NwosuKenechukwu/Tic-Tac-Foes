f828c710dedfdb96908a26ee024528c667f1cbd9
NwosuKenechukwu <kenechukwugn@gmail.com>
2022-12-13 23:43:07 +0000

    Added Structure, Styling and Some Functionality
    Added event listeners to grids and the new game button. Added function to clear the board.

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+// ------------------------- VARIABLES
+const grids = document.querySelectorAll(".grid");
+const newGame = document.querySelector(".new-game");
+
+// ------------------------- MODULES AND FACTORIES
+const gameBoard = (() => {
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  return { board };
+})();
+
+const displayController = (() => {})();
+
+const player = (name) => {
+  return { name };
+};
+
+// ------------------------- FUNCTIONS
+const clearBoard = function () {
+  grids.forEach((grid) => {
+    grid.innerHTML = "";
+  });
+};
+
+// ------------------------- EVENT LISTENERS
+grids.forEach((grid) => {
+  grid.addEventListener("click", function (e) {
+    grid.value = "X";
+    grid.innerHTML = "X";
+    console.log(e);
+  });
+});
+
+newGame.addEventListener("click", function () {
+  clearBoard();
+});
