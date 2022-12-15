4a5a5ef1df88a286ef5c75313856d696dc69beac
NwosuKenechukwu <kenechukwugn@gmail.com>
2022-12-15 23:27:11 +0000

    Added player select Screen; Added link to Game board

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 "use strict";
 
 // ------------------------- VARIABLES
+const gameArea = document.querySelector(".game-area");
+const playerSelect = document.querySelector(".player-select");
+const p1 = document.querySelector(".player1");
+const p2 = document.querySelector(".player2");
+const board = document.querySelector(".board");
 const grids = document.querySelectorAll(".grid");
 const newGame = document.querySelector(".new-game");
+const resetGame = document.querySelector(".reset-game");
+let botBtn = document.querySelector(".bot-button");
+const selectBtns = document.querySelectorAll(".select-button");
+const resetOrNew = document.querySelector(".reset-or-new");
 
 // ------------------------- MODULES AND FACTORIES
 const gameBoard = (() => {
-  let board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  let board = [["", "", "", "", "", "", "", "", ""]];
 
   return { board };
 })();
 
 const displayController = (() => {})();
 
-const player = (name) => {
-  return { name };
+const player = (name, char) => {
+  return { name, char };
 };
 
 // ------------------------- FUNCTIONS
@@ -28,15 +33,41 @@ const clearBoard = function () {
   });
 };
 
-// ------------------------- EVENT LISTENERS
-grids.forEach((grid) => {
+const createBoard = function (player1, player2) {
+  p1.textContent = player1;
+  p2.textContent = player2;
+  playerSelect.style.display = "none";
+  resetOrNew.style.display = board.style.display = "flex";
+};
+
+const reloadGame = function () {
+  clearBoard();
+  playerSelect.style.display = "flex";
+  resetOrNew.style.display = board.style.display = "none";
+};
+
+// -------------------------EVENT LISTENERS
+
+grids.forEach((grid, i) => {
   grid.addEventListener("click", function (e) {
-    grid.value = "X";
-    grid.innerHTML = "X";
-    console.log(e);
+    console.log(i);
+    grid.value = board[i] = "X";
+    grid.innerHTML = board[i] = "X";
   });
 });
 
-newGame.addEventListener("click", function () {
-  clearBoard();
+resetGame.addEventListener("click", clearBoard);
+
+newGame.addEventListener("click", reloadGame);
+
+selectBtns.forEach((selectBtn) => {
+  selectBtn.addEventListener("click", function (e) {
+    if (e.target.classList.contains("player-vs-bot")) {
+      createBoard("Player", "Tic-Tac-Foe");
+      botBtn.style.visibility = "hidden";
+    } else {
+      createBoard("Player 1", "Player 2");
+      botBtn.style.visibility = "visible";
+    }
+  });
 });
