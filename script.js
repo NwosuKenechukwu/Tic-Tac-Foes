6a3d485bb13bb7d18ecd99d4f1747bd8bfa195b6
NwosuKenechukwu <kenechukwugn@gmail.com>
2022-12-17 22:50:32 +0000

    Added name change functionality. Added limited turn logic
    Added the ability to change and confirm name; Added logic to hide certain buttons once play has started and also reveal buttons on board reset/new game

--- script.js
+++ script.js
@@ -23,15 +23,31 @@ let cells = [["", "", "", "", "", "", "", "", ""]];
 
 const displayController = (() => {})();
 
-const player = (name, char) => {
-  return { name, char };
+const player = (name, sign, turn) => {
+  return { name, sign, turn };
 };
+
+let curr, next;
+
+// ------------------------- PLAYERS
+
+const player1 = player("Player", "X", true);
+const player2 = player("Player", "0", false);
+const bot = player("Tic Tac Foe", "0", false);
 
 // ------------------------- FUNCTIONS
 const clearBoard = function () {
   grids.forEach((grid, i) => {
+    showBtns();
     grid.innerHTML = "";
     cells[i] = "";
+    if (curr.sign === "X") {
+      curr.turn = true;
+      next.turn = false;
+    } else {
+      curr.turn = false;
+      next.turn = true;
+    }
   });
 };
 
@@ -43,50 +59,104 @@ const createBoard = function (player1, player2) {
 };
 
 const reloadGame = function () {
+  showBtns();
   clearBoard();
   playerSelect.style.display = "flex";
   resetOrNew.style.display = board.style.display = "none";
 };
 
-const swapSigns = function () {
-  if (signX.textContent === "X") {
-    signX.value = signX.textContent = "0";
-    signO.value = signO.textContent = "X";
-  } else {
-    signX.value = signX.textContent = "X";
-    signO.value = signO.textContent = "O";
-  }
+const hideBtns = function () {
+  changeNameBtns.forEach((changeNameBtn) => {
+    changeNameBtn.style.visibility = "hidden";
+  });
+  swapBtn.style.visibility = "hidden";
 };
 
-const changeName = function () {
-  console.log(
-    changeNameBtns[0].previousElementSibling.classList.contains("player1")
-  );
+const showBtns = function () {
+  changeNameBtns.forEach((changeNameBtn) => {
+    changeNameBtn.previousElementSibling.textContent === "Tic Tac Foe"
+      ? (changeNameBtn.style.visibility = "hidden")
+      : (changeNameBtn.style.visibility = "visible");
+  });
+  swapBtn.style.visibility = "visible";
 };
-changeName();
 
 // -------------------------EVENT LISTENERS
 
 grids.forEach((grid, i) => {
   grid.addEventListener("click", function (e) {
-    grid.value = cells[i] = "X";
-    grid.innerHTML = cells[i] = "X";
+    hideBtns();
+    if (grid.textContent !== "") return;
+    if (curr.turn === true) {
+      curr.turn = false;
+      next.turn = true;
+      grid.value = cells[i] = curr.sign;
+      grid.innerHTML = cells[i] = curr.sign;
+    } else {
+      next.turn = false;
+      curr.turn = true;
+      grid.value = cells[i] = next.sign;
+      grid.innerHTML = cells[i] = next.sign;
+    }
+
+    changeNameBtns.forEach((changeNameBtn) => {
+      if (changeNameBtn.textContent === "OK") {
+        (changeNameBtn.previousElementSibling.classList.contains("player1")
+          ? p1
+          : p2
+        ).textContent =
+          document.querySelector(".input-field").value === ""
+            ? "Player"
+            : document.querySelector(".input-field").value;
+        changeNameBtn.textContent = "Change Name";
+      }
+    });
   });
 });
 
 resetGame.addEventListener("click", clearBoard);
 
-swapBtn.addEventListener("click", swapSigns);
+swapBtn.addEventListener("click", function () {
+  if (p2.textContent !== "Tic Tac Toe") {
+    if (signX.textContent === "X") {
+      signO.value = signO.textContent = next.sign = "X";
+      signX.value = signX.textContent = curr.sign = "0";
+      next.turn = true;
+      curr.turn = false;
+    } else {
+      signX.value = signX.textContent = curr.sign = "X";
+      signO.value = signO.textContent = next.sign = "0";
+      curr.turn = true;
+      next.turn = false;
+    }
+  } else {
+    if (signX.textContent === "X") {
+      signO.value = signO.textContent = next.sign = "X";
+      signX.value = signX.textContent = curr.sign = "0";
+      next.turn = true;
+      curr.turn = false;
+    } else {
+      signX.value = signX.textContent = curr.sign = "X";
+      signO.value = signO.textContent = next.sign = "0";
+      curr.turn = true;
+      next.turn = false;
+    }
+  }
+});
 
 newGame.addEventListener("click", reloadGame);
 
 selectBtns.forEach((selectBtn) => {
   selectBtn.addEventListener("click", function (e) {
     if (e.target.classList.contains("player-vs-bot")) {
-      createBoard("Player", "Tic-Tac-Foe");
+      createBoard(player1.name, bot.name);
+      curr = player1;
+      next = bot;
       botBtn.style.visibility = "hidden";
     } else {
-      createBoard("Player 1", "Player 2");
+      createBoard(player1.name, player2.name);
+      curr = player1;
+      next = player2;
       botBtn.style.visibility = "visible";
     }
   });
@@ -94,7 +164,24 @@ selectBtns.forEach((selectBtn) => {
 
 changeNameBtns.forEach((changeNameBtn) => {
   changeNameBtn.addEventListener("click", function () {
-    if (changeNameBtn.previousElementSibling.classList.contains("player1")) {
+    if (changeNameBtn.textContent === "OK") {
+      (changeNameBtn.previousElementSibling.classList.contains("player1")
+        ? p1
+        : p2
+      ).textContent =
+        document.querySelector(".input-field").value === ""
+          ? "Player"
+          : document.querySelector(".input-field").value;
+      changeNameBtn.textContent = "Change Name";
+    } else if (
+      changeNameBtn.previousElementSibling.classList.contains("player-name") &&
+      changeNameBtn.textContent !== "OK"
+    ) {
+      changeNameBtn.previousElementSibling.classList.contains("player1")
+        ? (p1.innerHTML = `<input type="text" class="input-field" />`)
+        : (p2.innerHTML = `<input type="text" class="input-field" />`);
+      document.querySelector(".input-field").focus();
+      changeNameBtn.textContent = "OK";
     }
   });
 });
