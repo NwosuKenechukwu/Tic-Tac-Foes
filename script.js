f49d16155bf57db8ef97688a474f68775a2cd96e
NwosuKenechukwu <kenechukwugn@gmail.com>
2022-12-19 23:54:09 +0000

    Fixed Bugs. Added Win Check Logic. Refactored Code a bit
    Added win check for horizontal, vertical and diagonal cases. Fixed bug that did not recognise win logic

--- script.js
+++ script.js
@@ -31,13 +31,14 @@ let curr, next;
 
 // ------------------------- PLAYERS
 
-const player1 = player("Player", "X", true);
-const player2 = player("Player", "0", false);
+const player1 = player("Player 1", "X", true);
+const player2 = player("Player 2", "0", false);
 const bot = player("Tic Tac Foe", "0", false);
 
 // ------------------------- FUNCTIONS
 const clearBoard = function () {
   cells = new Array(9);
+  count = 0;
 
   grids.forEach((grid, i) => {
     showBtns();
@@ -60,6 +61,9 @@ const createBoard = function (player1, player2) {
 };
 
 const reloadGame = function () {
+  changeNameBtns.forEach((changeNameBtn) => {
+    setName(changeNameBtn);
+  });
   showBtns();
   clearBoard();
   playerSelect.style.display = "flex";
@@ -82,28 +86,95 @@ const showBtns = function () {
   swapBtn.style.visibility = "visible";
 };
 
+const swapPlayer = function () {
+  if (next === bot || curr === bot) {
+    if (curr === player1) {
+      next = player1;
+      curr = bot;
+    } else {
+      next = bot;
+      curr = player1;
+    }
+  } else {
+    if (curr === player1) {
+      next = player1;
+      curr = player2;
+    } else {
+      next = player2;
+      curr = player1;
+    }
+  }
+};
+
+const swapSigns = function () {
+  signO.classList.toggle("sign-o");
+  signO.classList.toggle("sign-x");
+  signX.classList.toggle("sign-o");
+  signX.classList.toggle("sign-x");
+
+  swapPlayer();
+
+  if (signX.textContent === "X") {
+    signO.value = signO.textContent = "X";
+    signX.value = signX.textContent = "0";
+    curr.sign = "X";
+    next.sign = "0";
+    next.turn = false;
+    curr.turn = true;
+  } else {
+    signX.value = signX.textContent = "X";
+    signO.value = signO.textContent = "0";
+    curr.sign = "X";
+    next.sign = "0";
+    curr.turn = true;
+    next.turn = false;
+  }
+};
+
+const setName = function (btn) {
+  if (next === bot || curr === bot) {
+    if (curr === player1) {
+      curr.name = p1.textContent;
+      next.name = p2.textContent;
+    } else {
+      next.name = p1.textContent;
+      curr.name = p2.textContent;
+    }
+  } else {
+    if (curr === player1) {
+      curr.name = p1.textContent;
+      next.name = p2.textContent;
+    } else {
+      next.name = p1.textContent;
+      curr.name = p2.textContent;
+    }
+  }
+
+  if (btn) btn.textContent = "Change Name";
+  if (p1.textContent === "") p1.textContent = "Player 1";
+  if (p2.textContent === "") p2.textContent = "Player 2";
+};
+
 const confirmName = function (btn) {
   (btn.previousElementSibling.classList.contains("player1")
     ? p1
     : p2
-  ).textContent =
-    document.querySelector(".input-field").value === ""
-      ? "Player"
-      : document.querySelector(".input-field").value;
-  btn.textContent = "Change Name";
+  ).textContent = document.querySelector(".input-field").value;
+
+  setName(btn);
 };
 
 const changeName = function (btn) {
   if (btn.previousElementSibling.classList.contains("player1")) {
     p1.innerHTML = `<input type="text" class="input-field" />`;
     if (p2.nextElementSibling.textContent === "OK") {
-      p2.textContent = "Player";
+      p2.textContent = "Player 2";
       p2.nextElementSibling.textContent = "Change Name";
     }
   } else {
     p2.innerHTML = `<input type="text" class="input-field" />`;
     if (p1.nextElementSibling.textContent === "OK") {
-      p1.textContent = "Player";
+      p1.textContent = "Player 1";
       p1.nextElementSibling.textContent = "Change Name";
     }
   }
@@ -111,18 +182,60 @@ const changeName = function (btn) {
   btn.textContent = "OK";
 };
 
-const checkHorizontal = function () {
-  cells
-    .filter((_, i) => i < 3)
-    .forEach((cell, i) => {
-      if (cell === cells[i + 3] && cell === cells[i + 6]) endGame(cell);
-    });
+const checkVertical = function () {
+  for (let i = 0; i < 3; i++) {
+    if (
+      cells[i] !== undefined &&
+      cells[i] === cells[i + 3] &&
+      cells[i] === cells[i + 6]
+    ) {
+      endGame(cells[i]);
+    }
+  }
 };
 
+const checkHorizontal = function () {
+  for (let i = 0; i < 7; i += 3) {
+    if (
+      cells[i] !== undefined &&
+      cells[i] === cells[i + 1] &&
+      cells[i] === cells[i + 2]
+    ) {
+      endGame(cells[i]);
+    }
+  }
+};
+
+const checkDiagonal = function () {
+  for (let i = 0; i < 3; i += 2) {
+    if (
+      cells[i] !== undefined &&
+      cells[i] === cells[i + 4] &&
+      cells[i] === cells[i + 8] &&
+      i === 0
+    ) {
+      endGame(cells[i]);
+    }
+
+    if (
+      cells[i] !== undefined &&
+      cells[i] === cells[i + 2] &&
+      cells[i] === cells[i + 4] &&
+      i === 2
+    ) {
+      endGame(cells[i]);
+    }
+  }
+};
+
+let count = 0;
 const endGame = function (sign) {
+  if (count > 0) return;
   curr.sign === sign
     ? console.log(`${curr.name} Wins!`)
     : console.log(`${next.name} Wins!`);
+
+  count++;
 };
 
 // -------------------------EVENT LISTENERS
@@ -130,6 +243,10 @@ const endGame = function (sign) {
 grids.forEach((grid, i) => {
   grid.addEventListener("click", function (e) {
     hideBtns();
+
+    // if (swapBtn.style.visibility === "hidden") {
+    //   curr =
+    // }
 
     if (grid.textContent !== "") return;
     if (curr.turn === true) {
@@ -144,7 +261,9 @@ grids.forEach((grid, i) => {
       grid.innerHTML = cells[i] = next.sign;
     }
 
+    checkVertical();
     checkHorizontal();
+    checkDiagonal();
 
     {
       changeNameBtns.forEach((changeNameBtn) => {
@@ -157,31 +276,7 @@ grids.forEach((grid, i) => {
 resetGame.addEventListener("click", clearBoard);
 
 swapBtn.addEventListener("click", function () {
-  if (p2.textContent !== "Tic Tac Toe") {
-    if (signX.textContent === "X") {
-      signO.value = signO.textContent = next.sign = "X";
-      signX.value = signX.textContent = curr.sign = "0";
-      next.turn = true;
-      curr.turn = false;
-    } else {
-      signX.value = signX.textContent = curr.sign = "X";
-      signO.value = signO.textContent = next.sign = "0";
-      curr.turn = true;
-      next.turn = false;
-    }
-  } else {
-    if (signX.textContent === "X") {
-      signO.value = signO.textContent = next.sign = "X";
-      signX.value = signX.textContent = curr.sign = "0";
-      next.turn = true;
-      curr.turn = false;
-    } else {
-      signX.value = signX.textContent = curr.sign = "X";
-      signO.value = signO.textContent = next.sign = "0";
-      curr.turn = true;
-      next.turn = false;
-    }
-  }
+  swapSigns();
 });
 
 newGame.addEventListener("click", reloadGame);
@@ -191,12 +286,16 @@ selectBtns.forEach((selectBtn) => {
     if (e.target.classList.contains("player-vs-bot")) {
       createBoard(player1.name, bot.name);
       curr = player1;
+      signX.textContent = player1.sign;
       next = bot;
+      signO.textContent = bot.sign;
       botBtn.style.visibility = "hidden";
     } else {
       createBoard(player1.name, player2.name);
       curr = player1;
+      signX.textContent = player1.sign;
       next = player2;
+      signO.textContent = player2.sign;
       botBtn.style.visibility = "visible";
     }
   });
