904e939279407638d8204556089e1c82b69d2698
NwosuKenechukwu <kenechukwugn@gmail.com>
2023-01-11 23:51:07 +0000

    Finished Minimax AI implementation as well as styling

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let botBtn = document.querySelector(".bot-button");
 const selectBtns = document.querySelectorAll(".select-button");
 const resetOrNew = document.querySelector(".reset-or-new");
 const changeNameBtns = document.querySelectorAll(".change-name");
+const difficulty = document.querySelector(".diff");
+const difficultyText = document.querySelector(".diffText");
 const signX = document.querySelector(".sign-x");
 const signO = document.querySelector(".sign-o");
 const outcome = document.querySelector(".outcome");
@@ -42,20 +44,38 @@ const bot = player("Tic Tac Foe", "0", false);
 const clearBoard = function () {
   cells = new Array(9);
   cellIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  randomCell = cellIndex[Math.floor(Math.random() * cellIndex.length)];
   count = 0;
+  winner = null;
+  difficulty.value = "placeholder";
+  difficultyText.style.display = "none";
+  difficulty.style.display = "block";
+
+  showBtns();
 
   grids.forEach((grid, i) => {
-    showBtns();
     grid.innerHTML = "";
-    if (curr.sign === "X") {
-      curr.turn = true;
-      next.turn = false;
-    } else {
-      curr.turn = false;
-      next.turn = true;
-    }
   });
+  curr.sign = "X";
+  next.sign = "0";
+  curr.turn = true;
+  next.turn = false;
+
+  if (curr === bot) {
+    difficultyText.style.display = "block";
+    difficultyText.textContent = `${
+      difficulty.value.charAt(0).toUpperCase() + difficulty.value.slice(1)
+    } Difficulty`;
+
+    hideBtns();
+    setTimeout(() => {
+      if (difficulty.value === "easy" || difficulty.value === "placeholder") {
+        difficulty.value = "easy";
+        botMove();
+      } else {
+        bestBotMove(true);
+      }
+    }, 800);
+  }
 };
 
 const createBoard = function (player1, player2) {
@@ -64,7 +84,6 @@ const createBoard = function (player1, player2) {
   playerSelect.style.display = "none";
   swapBtn.style.display = resetGame.style.display = "block";
   resetOrNew.style.display = board.style.display = "flex";
-  if (curr === bot) swapSigns();
 };
 
 const reloadGame = function () {
@@ -82,6 +101,7 @@ const hideBtns = function () {
   changeNameBtns.forEach((changeNameBtn) => {
     changeNameBtn.style.visibility = "hidden";
   });
+  difficulty.style.visibility = "hidden";
   swapBtn.style.visibility = "hidden";
 };
 
@@ -91,6 +111,7 @@ const showBtns = function () {
       ? (changeNameBtn.style.visibility = "hidden")
       : (changeNameBtn.style.visibility = "visible");
   });
+  difficulty.style.visibility = "visible";
   swapBtn.style.visibility = "visible";
 };
 
@@ -138,13 +159,35 @@ const swapSigns = function () {
     next.turn = false;
   }
 
+  if (difficulty.value === "easy" || difficulty.value === "placeholder") {
+    difficulty.value = "easy";
+  }
+
   hideBtns();
-  setTimeout(() => {
-    botMove();
-  }, 800);
+  if (curr === bot) {
+    difficulty.style.display = "none";
+    difficultyText.style.display = "block";
+    difficultyText.textContent = `${
+      difficulty.value.charAt(0).toUpperCase() + difficulty.value.slice(1)
+    } Difficulty`;
+
+    setTimeout(() => {
+      if (difficulty.value === "easy") {
+        difficulty.value = "easy";
+        botMove();
+      } else {
+        bestBotMove(true);
+      }
+    }, 800);
+  }
 };
 
 const setName = function (btn) {
+  if ((btn.textContent = "Change Name")) {
+    if (p1.textContent === "") p1.textContent = "Player 1";
+    if (p2.textContent === "") p2.textContent = "Player 2";
+  }
+
   if (next === bot || curr === bot) {
     if (curr === player1) {
       curr.name = p1.textContent;
@@ -162,10 +205,6 @@ const setName = function (btn) {
       curr.name = p2.textContent;
     }
   }
-
-  if (btn) btn.textContent = "Change Name";
-  if (p1.textContent === "") p1.textContent = "Player 1";
-  if (p2.textContent === "") p2.textContent = "Player 2";
 };
 
 const confirmName = function (btn) {
@@ -195,6 +234,10 @@ const changeName = function (btn) {
   btn.textContent = "OK";
 };
 
+const checkIfEmpty = function (cell) {
+  return cells[cell] === undefined;
+};
+
 const checkVertical = function () {
   for (let i = 0; i < 3; i++) {
     if (
@@ -202,7 +245,7 @@ const checkVertical = function () {
       cells[i] === cells[i + 3] &&
       cells[i] === cells[i + 6]
     ) {
-      endGame(cells[i]);
+      winner = cells[i];
     }
   }
 };
@@ -214,7 +257,7 @@ const checkHorizontal = function () {
       cells[i] === cells[i + 1] &&
       cells[i] === cells[i + 2]
     ) {
-      endGame(cells[i]);
+      winner = cells[i];
     }
   }
 };
@@ -227,7 +270,7 @@ const checkDiagonal = function () {
       cells[i] === cells[i + 8] &&
       i === 0
     ) {
-      endGame(cells[i]);
+      winner = cells[i];
     }
 
     if (
@@ -236,20 +279,39 @@ const checkDiagonal = function () {
       cells[i] === cells[i + 4] &&
       i === 2
     ) {
-      endGame(cells[i]);
+      winner = cells[i];
     }
   }
 };
 
+const checkWinner = function () {
+  winner = null;
+  checkHorizontal();
+  checkVertical();
+  checkDiagonal();
+  checkDraw();
+  if (typeof winner !== "string") {
+    return null;
+  } else {
+    return winner;
+  }
+};
+
 const checkDraw = function () {
-  if (cells.filter((cell) => cell !== undefined).length === 9 && count === 0) {
-    endGame("Draw");
+  if (
+    cells.filter((cell) => cell !== undefined).length === 9 &&
+    count === 0 &&
+    winner === null
+  ) {
+    winner = "Draw";
   }
 };
 
 let count = 0;
+let winner = null;
 const endGame = function (sign) {
   if (count > 0) return;
+  winner = sign;
 
   count++;
 
@@ -270,36 +332,124 @@ const endGame = function (sign) {
   }
 };
 
-let randomCell = cellIndex[Math.floor(Math.random() * cellIndex.length)];
 const botMove = function () {
-  randomCell = cellIndex[Math.floor(Math.random() * cellIndex.length)];
+  let randomCell = cellIndex[Math.floor(Math.random() * cellIndex.length)];
   if (next === bot && next.turn === true) {
-    if (cells[randomCell] === undefined) {
+    if (checkIfEmpty(randomCell)) {
       next.turn = false;
       curr.turn = true;
-      grids[randomCell].value = cells[randomCell] = next.sign;
-      grids[randomCell].innerHTML = cells[randomCell] = next.sign;
+      grids[randomCell].value =
+        grids[randomCell].innerHTML =
+        cells[randomCell] =
+          next.sign;
       cellIndex.splice(cellIndex.indexOf(randomCell), 1);
     }
-    console.log(
-      "Bot Selected: ",
-      randomCell,
-      cellIndex[randomCell] === undefined
-    );
   } else if (curr === bot && curr.turn === true) {
-    if (cells[randomCell] === undefined) {
+    if (checkIfEmpty(randomCell)) {
       curr.turn = false;
       next.turn = true;
-      grids[randomCell].value = cells[randomCell] = curr.sign;
-      grids[randomCell].innerHTML = cells[randomCell] = curr.sign;
+      grids[randomCell].value =
+        grids[randomCell].innerHTML =
+        cells[randomCell] =
+          curr.sign;
       cellIndex.splice(cellIndex.indexOf(randomCell), 1);
     }
-    console.log(
-      "Bot Selected: ",
-      randomCell,
-      cellIndex[randomCell] === undefined
-    );
   }
+};
+
+let scoresTable = {
+  X: 10,
+  0: -10,
+  Draw: 0,
+};
+
+const minimax = function (board, depth, isMaximizing) {
+  let result = checkWinner();
+  if (result !== null) return scoresTable[result] + depth;
+
+  if (next === bot) {
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 9; i++) {
+        if (board[i] === undefined) {
+          board[i] = player1.sign;
+          let score = minimax(board, depth + 1, false);
+          board[i] = undefined;
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 9; i++) {
+        if (board[i] === undefined) {
+          board[i] = bot.sign;
+          let score = minimax(board, depth + 1, true);
+          board[i] = undefined;
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+      return bestScore;
+    }
+  } else {
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 9; i++) {
+        if (board[i] === undefined) {
+          board[i] = bot.sign;
+          let score = minimax(board, depth + 1, false);
+          board[i] = undefined;
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 9; i++) {
+        if (board[i] === undefined) {
+          board[i] = player1.sign;
+          let score = minimax(board, depth + 1, true);
+          board[i] = undefined;
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+      return bestScore;
+    }
+  }
+};
+
+const bestBotMove = function (starting) {
+  let bestScore = starting ? -Infinity : Infinity;
+  let move;
+  for (let i = 0; i < 9; i++) {
+    if (cells[i] === undefined) {
+      cells[i] = bot.sign;
+      let score = starting ? minimax(cells, 0, false) : minimax(cells, 0, true);
+      cells[i] = undefined;
+      if (starting) {
+        if (score > bestScore) {
+          bestScore = score;
+          move = i;
+        }
+      } else {
+        if (score < bestScore) {
+          bestScore = score;
+          move = i;
+        }
+      }
+    }
+  }
+
+  bot.turn = false;
+  player1.turn = true;
+  grids[move].value = grids[move].innerHTML = cells[move] = bot.sign;
+
+  setTimeout(function () {
+    checkWinner();
+    if (winner !== null) endGame(winner);
+  }, 610);
+
+  cellIndex.splice(cellIndex.indexOf(move), 1);
 };
 
 // -------------------------EVENT LISTENERS
@@ -307,71 +457,55 @@ const botMove = function () {
 grids.forEach((grid, i) => {
   grid.addEventListener("click", function (e) {
     hideBtns();
-    console.log(randomCell);
+
+    if (difficulty.value === "easy" || difficulty.value === "placeholder") {
+      difficulty.value = "easy";
+    }
+    difficulty.style.display = "none";
+    difficultyText.style.display = "block";
+    difficultyText.textContent = `${
+      difficulty.value.charAt(0).toUpperCase() + difficulty.value.slice(1)
+    } Difficulty`;
 
     if (grid.textContent !== "") return;
     if (next === player2 || curr === player2) {
       if (curr.turn === true) {
         curr.turn = false;
         next.turn = true;
-        grid.value = cells[i] = curr.sign;
-        grid.innerHTML = cells[i] = curr.sign;
+        grid.value = grid.innerHTML = cells[i] = curr.sign;
       } else {
         next.turn = false;
         curr.turn = true;
-        grid.value = cells[i] = next.sign;
-        grid.innerHTML = cells[i] = next.sign;
+        grid.value = grid.innerHTML = cells[i] = next.sign;
       }
     }
 
     if (next === bot || curr === bot) {
-      if (next === bot) {
-        if (curr.turn === true) {
-          curr.turn = false;
-          next.turn = true;
-          grid.value = cells[i] = curr.sign;
-          grid.innerHTML = cells[i] = curr.sign;
-          cellIndex.splice(cellIndex.indexOf(i), 1);
-          console.log(
-            "Player Selected: ",
-            randomCell,
-            cellIndex[randomCell] === undefined
-          );
-          if (cells.filter((cell) => cell !== undefined).length !== 9) {
-            setTimeout(function () {
+      if (player1.turn === true) {
+        bot.turn = true;
+        player1.turn = false;
+        grid.value = grid.innerHTML = cells[i] = player1.sign;
+        cellIndex.splice(cellIndex.indexOf(i), 1);
+        if (cells.filter((cell) => cell !== undefined).length !== 9) {
+          setTimeout(function () {
+            if (
+              difficulty.value === "easy" ||
+              difficulty.value === "placeholder"
+            ) {
+              difficulty.value = "easy";
               botMove();
-            }, 600);
-          }
-        }
-      } else if (curr === bot) {
-        if (next.turn === true) {
-          curr.turn = true;
-          next.turn = false;
-          grid.value = cells[i] = next.sign;
-          grid.innerHTML = cells[i] = next.sign;
-          cellIndex.splice(cellIndex.indexOf(i), 1);
-          console.log(
-            "Player Selected: ",
-            randomCell,
-            cellIndex[randomCell] === undefined
-          );
-          if (cells.filter((cell) => cell !== undefined).length !== 9) {
-            setTimeout(function () {
-              botMove();
-            }, 600);
-          }
+            } else {
+              bestBotMove(false);
+            }
+          }, 600);
         }
       }
     }
 
     setTimeout(function () {
-      checkVertical();
-      checkHorizontal();
-      checkDiagonal();
-      checkDraw();
+      checkWinner();
+      if (winner !== null) endGame(winner);
     }, 610);
-
-    console.log(cellIndex);
 
     changeNameBtns.forEach((changeNameBtn) => {
       if (changeNameBtn.textContent === "OK") confirmName(changeNameBtn);
@@ -390,19 +524,27 @@ newGame.addEventListener("click", reloadGame);
 selectBtns.forEach((selectBtn) => {
   selectBtn.addEventListener("click", function (e) {
     if (e.target.classList.contains("player-vs-bot")) {
+      player1.name = "Player 1";
       createBoard(player1.name, bot.name);
       curr = player1;
-      signX.textContent = player1.sign;
       next = bot;
-      signO.textContent = bot.sign;
-      botBtn.style.visibility = "hidden";
+      clearBoard();
+      signX.textContent = curr.sign;
+      signO.textContent = next.sign;
+      botBtn.style.display = "none";
+      difficulty.style.display = "block";
     } else {
+      player1.name = "Player 1";
+      player2.name = "Player 2";
       createBoard(player1.name, player2.name);
       curr = player1;
-      signX.textContent = player1.sign;
       next = player2;
-      signO.textContent = player2.sign;
-      botBtn.style.visibility = "visible";
+      clearBoard();
+      signX.textContent = curr.sign;
+      signO.textContent = next.sign;
+      botBtn.style.display = "block";
+      difficulty.style.display = "none";
+      difficultyText.style.display = "none";
     }
     showBtns();
   });
